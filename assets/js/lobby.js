153b14371720e00eccbc6cc07b723551a4795644
Luis Jaimes <luisalbertoj.tober@gmail.com>
2021-08-09 09:52:52 -0500

    menu change

--- assets/js/lobby.js
+++ assets/js/lobby.js
@@ -1,14 +1,17 @@
 
 const btnSalir = document.getElementById('btnSalir');
+const user = JSON.parse(localStorage.getItem('user'));
+const fullname = document.getElementById('fullname');
+fullname.innerHTML = `${user.name} ${user.lastname}`;
 btnSalir.addEventListener('click', (evt) => {
     Swal.fire({
-    title: 'Cerrar Sesión!',
-    text: '¡Vuelve pronto!',
-    icon: 'info',
-    confirmButtonText: 'Salir'
-}).then((result) => {
-    localStorage.removeItem('loggedIn');
-    window.location.href = "login.html";
-});
+        title: 'Cerrar Sesión!',
+        text: '¡Vuelve pronto!',
+        icon: 'info',
+        confirmButtonText: 'Salir'
+    }).then((result) => {
+        localStorage.removeItem('loggedIn');
+        window.location.href = "login.html";
+    });
 });
 
